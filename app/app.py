52d00ffa0178276664e12c6486ea92f270b7a5a7
CarlosACC05 <124701900+CarlosACC05@users.noreply.github.com>
2026-04-14 18:47:39 -0600

    Add files via upload

--- app/app.py
+++ app/app.py
@@ -1,18 +1,23 @@
 # Se importa la clase flask de la propia libreria de flask, 
 # es la clase encargada de levantar la aplicacion, el servidor no funciona sin este importe.
+from dotenv import load_dotenv
+load_dotenv()
+
 from flask import Flask, render_template, redirect, url_for, request
 from flask_mysqldb import MySQL
+from config import Config
 
 # Creacion de la instancia de aplicación, _name_ es una varible estatica especial de python
 # le dice a flask donde esta ubicado el archivo principal y donde buscar recursos. Nunca cambia.
 app = Flask(__name__)
+app.config.from_object(Config)
 
 # Conexion a MYSQL
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'donaciones'
+#app.config['MYSQL_HOST'] = 'localhost'
+#app.config['MYSQL_USER'] = 'root'
+#app.config['MYSQL_PASSWORD'] = 'root'
+#app.config['MYSQL_DB'] = 'donaciones'
 
 conexion = MySQL(app)
 
@@ -116,7 +121,7 @@ def index():
         centros = []
 
     # 4) Mandar centros al HTML
-    return render_template("index.html", data=data, centros=centros)
+    return render_template("index.html", data=data, centros=centros, api_key=app.config["API_KEY"])
 
 def pagina_no_encontrada(error):
     return render_template('404.html'), 404
