91baaa1d1d18ad9d8f4adf03aacaf4243e808180
CarlosACC05 <124701900+CarlosACC05@users.noreply.github.com>
2026-04-14 22:31:51 -0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 from flask import Flask, render_template, redirect, url_for, request
 from flask_mysqldb import MySQL
-from .config import Config
+from config import Config
 
 # Creacion de la instancia de aplicación, _name_ es una varible estatica especial de python
 # le dice a flask donde esta ubicado el archivo principal y donde buscar recursos. Nunca cambia.
@@ -35,11 +35,93 @@ def limpiar_categoria(cat):
 @app.route("/")
 # Funcion normal de python, lo que sea que esta funcion haga es lo que vera el usuario en el navegador
 def index():
+    # return '<h1 style="color: blue;">Hola Carlos, tu servidor Flask está funcionando.<h1>'
+    categorias = [
+        "Todas",
+        "Donaciones Económicas",
+        "Ropa",
+        "Alimentos y Despensas",
+        "Juguetes",
+        "Materiales para reciclar",
+        "Medicamentos",
+        "Otros"
+    ]
+    
+    data = {
+        'titulo': 'Despensas Bienestar',
+        'bienvenida': 'Bienvenido a Despensas Bienestar',
+        'numero_botones': len(categorias),
+        'botones': categorias
+        }
+
+    # Si existe un parametro llamado categoria en la url lo captura en categoria y lo imprime en la terminal
+    categoria = request.args.get("categoria")
+
     try:
         cursor = conexion.connection.cursor()
-        return "Conectado a MySQL"
-    except Exception as e:
-        return f"Error conexión: {e}"
+
+
+        if categoria and categoria != "Todas":
+
+            sql = """
+            SELECT 
+            centros.id_centro,
+            centros.nombre,
+            centros.descripcion,
+            centros.direccion,
+            centros.telefono,
+            centros.email,
+            centros.pagina_web,
+            categorias.nombre
+            FROM centros
+            JOIN centro_categoria 
+            ON centros.id_centro = centro_categoria.id_centro
+            JOIN categorias 
+            ON categorias.id_categoria = centro_categoria.id_categoria
+            WHERE categorias.nombre = %s
+            """
+
+            cursor.execute(sql, (categoria,))
+
+        else:
+
+            sql = """
+            SELECT 
+            centros.id_centro,
+            centros.nombre,
+            centros.descripcion,
+            centros.direccion,
+            centros.telefono,
+            centros.email,
+            centros.pagina_web,
+            GROUP_CONCAT(categorias.nombre SEPARATOR ', ') AS categorias
+            FROM centros
+            JOIN centro_categoria 
+            ON centros.id_centro = centro_categoria.id_centro
+            JOIN categorias 
+            ON categorias.id_categoria = centro_categoria.id_categoria
+            GROUP BY centros.id_centro
+            """
+            cursor.execute(sql)
+        centros = cursor.fetchall()
+        print(centros)
+
+        cursor.close()
+
+        centros = [
+        list(c) + [limpiar_categoria(c[7])]
+        for c in centros
+        ]
+        print(centros)
+
+
+    except Exception as ex:
+        print("ERROR SQL:", ex)
+        data['mensaje'] = 'Error'
+        centros = []
+
+    # Mandar centros al HTML
+    return render_template("index.html", data=data, centros=centros, api_key=app.config["API_KEY"])
 
 def pagina_no_encontrada(error):
     return render_template('404.html'), 404
